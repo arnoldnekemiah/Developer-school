3616826d3af45873e85fa59aeb38ef2e51940418
Ikonde Nekemiah Arnold <i-nad@localhost.localdomain>
2023-05-23 19:21:37 +0300

    Created about me section and made more changes

--- main.js
+++ main.js
@@ -32,43 +32,43 @@ const speakers = [
       name: 'Ikonde Arnold',
       image: './images/speaker1.png',
       alt: 'Speaker1',
-      intro: 'Berkman Professor of Entrepreneurial Legal Studies at Harvard Law School',
+      intro: 'Bachelor of Science in Computer Engineering',
       autobio: 'Benkler studies commons-based peer production, and published his seminal book, The Wealth of Networks in 2006'
     },
     {
       name: 'Beteise Richmond',
       image: './images/speaker2.png',
       alt: 'Speaker2',
-      intro: 'Director of Art Centre Nabi and a board member of CC Korea',
+      intro: 'Manager Web development at KCB Bank Uganda',
       autobio: 'As the main venue for new media art production in Korea, Nabi promotes cross-disciplinary collaboration and understanding among science technology, humanities, and the arts.'
     },
     {
         name: 'Josh Erias',
         image: './images/speaker3.png',
         alt: 'Speaker3',
-        intro: 'Director of Art Centre Nabi and a board member of CC Korea',
-        autobio: 'As the main venue for new media art production in Korea, Nabi promotes cross-disciplinary collaboration and understanding among science technology, humanities, and the arts.'
+        intro: 'Developer at Renu Uganda',
+        autobio: 'With the skills he obtained from several Bootcamps, he was able to stand up and rise through ranks'
     },
     {
         name: 'Mwarisi Arthur',
         image: './images/speaker4.png',
         alt: 'Speaker4',
         intro: 'Director of Art Centre Nabi and a board member of CC Korea',
-        autobio: 'As the main venue for new media art production in Korea, Nabi promotes cross-disciplinary collaboration and understanding among science technology, humanities, and the arts.'
+        autobio: 'As the main venue for new media art production in Korea, Nabi promotes cross-disciplinary.'
     },
     {
         name: 'Nantale Tracy',
         image: './images/speaker5.png',
         alt: 'Speaker5',
         intro: 'Director of Art Centre Nabi and a board member of CC Korea',
-        autobio: 'As the main venue for new media art production in Korea, Nabi promotes cross-disciplinary collaboration and understanding among science technology, humanities, and the arts.'
+        autobio: 'As the main venue for new media art production in Korea, Nabi promotes cross-disciplinary.'
       },
     {
         name: 'Masika Jackie',
         image: './images/speaker6.png',
         alt: 'Speaker6',
         intro: 'Director of Art Centre Nabi and a board member of CC Korea',
-        autobio: 'As the main venue for new media art production in Korea, Nabi promotes cross-disciplinary collaboration and understanding among science technology, humanities, and the arts.'
+        autobio: 'As the main venue for new media art production in Korea, Nabi promotes, Hacker Ethusiast.'
       }    
   ];
   
